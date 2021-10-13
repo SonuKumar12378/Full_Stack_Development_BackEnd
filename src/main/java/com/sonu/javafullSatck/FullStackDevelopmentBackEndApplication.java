4e696e205b8ddd18d8741e323dc28ca238e9b02c
sonukumar12378 <sonu93.vtu.eee@gmail.com>
2021-10-13 11:37:45 +0530

    first commit

---
+++ src/main/java/com/sonu/javafullSatck/FullStackDevelopmentBackEndApplication.java
@@ -0,0 +1,14 @@
+package com.sonu.javafullSatck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackDevelopmentBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackDevelopmentBackEndApplication.class, args);
+		System.out.println("Rajkamal....................");
+	}
+
+}
---
+++ src/test/java/com/sonu/javafullSatck/FullStackDevelopmentBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonu.javafullSatck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackDevelopmentBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
